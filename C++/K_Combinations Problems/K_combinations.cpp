82fcb998976065d4d7fb33fd02192ea6c32c8dc1
tharindujc <tharindujc@gmail.com>
2022-11-05 12:04:06 +1100

    started coding
    Check the compiler and other technical requirements of the runtime environment for C++.

---
+++ C++/K_Combinations Problems/K_combinations.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, k;
+    cout<<"Enter values for n and k, where n>=k"<<endl;
+    cin>>n>>k;
+    cout<<"n value is "<<n<<"\n";
+    cout<<"k value is "<<k<<"\n";
+    return 0;
+}
